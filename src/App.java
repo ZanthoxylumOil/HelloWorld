ff276c7f97ece06a13f81c1d5a35ec375c1cdcce
ZanthoxylumOil <179842693+ZanthoxylumOil@users.noreply.github.com>
2025-08-28 00:10:19 +0800

    save1

---
+++ src/App.java
@@ -0,0 +1,16 @@
+/**这是我们第一次学习javadoc
+ * @author Huang
+ * @version jdk11.0
+ */
+
+/*
+单行注释和多行注释的作用：
+    ①对缩写的程序进行解释说明，增强可读性
+    ②调试所写的代码
+*/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
